acec02c4eba6b96896eb9018c8928f47c0478115
devfilip1 <filipimoliva022@gmail.com>
2024-09-06 20:22:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo: "Albatroz",
+        descricao: "Mergulhe em um mundo de sabores no Albatroz Sabores! Nossa paixão por ingredientes frescos e receitas tradicionais se transforma em esfihas irresistíveis, com uma variedade de recheios que vão te surpreender. Mas não para por aí! Oferecemos também outros salgados deliciosos para acompanhar aquele lanche especial. Venha nos visitar e embarque nessa deliciosa jornada gastronômica!",
+        link: "https://g.co/kgs/SJhxx9o"
+    },
+    {
+        titulo: "Blue Praia Bar",
+        descricao: "Desfrute da brisa do mar e da culinária contemporânea do Blue Praia Bar. Nosso cardápio oferece opções leves e saborosas, perfeitas para um dia relaxante à beira-mar.",
+        link: "https://exemplo.com/bluepraia"
+    },
+    {
+        titulo: "Bravo",
+        descricao: "Para os amantes de hambúrgueres artesanais, o Bravo é o lugar certo. Nossos burgers são feitos com ingredientes frescos e de alta qualidade, e você pode montar o seu com diversas opções de acompanhamentos.",
+        link: "https://exemplo.com/bravo"
+    },
+    {
+        titulo: "Red Burguer",
+        descricao: "Hambúrgueres suculentos e porções generosas. O Red Burguer é a sua melhor opção para um lanche rápido e saboroso.",
+        link: "https://exemplo.com/redburguer"
+    },
+    {
+        titulo: "Oliva",
+        descricao: "Saboreie as melhores pizzas da cidade no Oliva. Massas frescas, ingredientes de qualidade e um cardápio variado para todos os gostos.",
+        link: "https://exemplo.com/oliva"
+    }
+]
